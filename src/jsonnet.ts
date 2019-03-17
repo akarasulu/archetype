b79d27aa859457c3ff56e1310ac77dc1c8ea8a19
Alex Karasulu <akarasulu@optimal-dynamics.com>
2019-03-17 08:55:04 +0600

    formatting doc before tslint

--- src/jsonnet.ts
+++ src/jsonnet.ts
@@ -1,79 +1,80 @@
-var fs = require('fs');
-var exec = require('child_process').exec, child;
+var fs = require("fs");
+var exec = require("child_process").exec,
+  child;
 
 /**
  * A wrapper around the native executable. Could not get the libjsonnet.js
  * library generated and working. So unfortunately this imposes the need for
  * users to have to install jsonnet and makes this library essentially useless
  * in web environments.
- * 
+ *
  * Would be nice to use the same wrapper with libjsonnet.js once we figure
  * out how to work with it properly,
  */
 export class Jsonnet {
-    private outFile: string | null = null;
-    private libPaths: Array<string> = [];
-    private args: Map<string,string> = new Map();
-    private vars: Map<string,string> = new Map();
-    private inFileChanged: number = Number.NEGATIVE_INFINITY;
-    private outFileChanged: number = Number.NEGATIVE_INFINITY;
+  private outFile: string | null = null;
+  private libPaths: Array<string> = [];
+  private args: Map<string, string> = new Map();
+  private vars: Map<string, string> = new Map();
+  private inFileChanged: number = Number.NEGATIVE_INFINITY;
+  private outFileChanged: number = Number.NEGATIVE_INFINITY;
 
-    constructor(private inFile: string) {
-        // check file, record last mode timestamp
-        if (!fs.existsSync(inFile)) {
-            throw new Error("Input file " + inFile + " does not exist.");
-        }
-
-        var stats = fs.statSync(inFile);
-        this.inFileChanged = stats.mtimeMs;
-    }
-    
-    setOutFile(outFile: string) {
-        this.outFile = outFile;
-        if(fs.existsSync(outFile)) {
-            var stats = fs.statSync(outFile);
-            this.outFileChanged = stats.mtimeMs;
-        }
-        return this;
+  constructor(private inFile: string) {
+    // check file, record last mode timestamp
+    if (!fs.existsSync(inFile)) {
+      throw new Error("Input file " + inFile + " does not exist.");
     }
 
-    addLibPath(path: string) {
-        this.libPaths.push(path);
-        return this;
+    var stats = fs.statSync(inFile);
+    this.inFileChanged = stats.mtimeMs;
+  }
+
+  setOutFile(outFile: string) {
+    this.outFile = outFile;
+    if (fs.existsSync(outFile)) {
+      var stats = fs.statSync(outFile);
+      this.outFileChanged = stats.mtimeMs;
     }
+    return this;
+  }
 
-    addArgs(key: string, value: string) {
-        this.args.set(key, value);
-        return this;
-    }
+  addLibPath(path: string) {
+    this.libPaths.push(path);
+    return this;
+  }
 
-    addVars(key: string, value: string) {
-        this.vars.set(key, value);
-        return this;
-    }
+  addArgs(key: string, value: string) {
+    this.args.set(key, value);
+    return this;
+  }
 
-    execute() {
-        var cmd = 'jsonnet'
-        this.libPaths.forEach(element => {
-            cmd += ' -J ' + element;
-        });
+  addVars(key: string, value: string) {
+    this.vars.set(key, value);
+    return this;
+  }
 
-        this.args.forEach((val: string, key: string) => {
-            cmd += ' -A ' + key + '="' + val + '"';
-        });
+  execute() {
+    var cmd = "jsonnet";
+    this.libPaths.forEach(element => {
+      cmd += " -J " + element;
+    });
 
-        this.vars.forEach((val: string, key: string) => {
-            cmd += ' -V ' + key + '="' + val + '"';
-        });
+    this.args.forEach((val: string, key: string) => {
+      cmd += " -A " + key + '="' + val + '"';
+    });
 
-        cmd += ' ' + this.inFile + ' -o ' + this.outFile
+    this.vars.forEach((val: string, key: string) => {
+      cmd += " -V " + key + '="' + val + '"';
+    });
 
-        child = exec(cmd, function (err: any, stdout: any, stderr: any) {
-            if(err !== null) {
-                console.log('exec error: ' + err);
-            }
-        });
-        
-        return this;
-    }
+    cmd += " " + this.inFile + " -o " + this.outFile;
+
+    child = exec(cmd, function(err: any, stdout: any, stderr: any) {
+      if (err !== null) {
+        console.log("exec error: " + err);
+      }
+    });
+
+    return this;
+  }
 }
